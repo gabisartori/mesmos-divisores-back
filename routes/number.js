4301e7858c15be52adbea9b2778cf82ce88b1733
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-08-08 00:46:17 -0300

    Corrigindo retorno da chamada

--- routes/number.js
+++ routes/number.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 function P(n){
     var total = 0;
-    for(var i = 1; i < n; i++){
+    for(var i = 1; i <= n; i++){
         if (n%i == 0){
             total++;
         }
@@ -14,13 +14,16 @@ function P(n){
 router.post('/:num', (req, res) => {
     try{
         let numbers = [];
+        let total = 0;
         let coiso = req.params.num;
         for(var i = 0; i < coiso; i++){
-            if (P(coiso) === P(i)){
+            if (P(i) === P(i+1)){
                 numbers.push(i);
+                total++;
             }
         }        
-        res.status(200).json(numbers);
+        //res.status(200).json([numbers, total]);
+        res.status(200).json(total);
     }catch (err){
         res.status(500).json({message: err.message});
     }
