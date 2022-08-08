609925d4707fe572fb6114d566b8a53a6d8a06c3
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-08-08 15:16:22 -0300

    enable cross origin
    Tentando

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.json());
+// app.use(express.json());
+app.use(
+    (req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    }
+)
+
 const solvesRouter = require('./routes/number');
 app.use('/number', solvesRouter);
 
